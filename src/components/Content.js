879d004b3b8b678686610ace0575db6e6855ddb0
Glen <gtaylor37@gmail.com>
2021-06-15 12:27:22 -0400

    Outline the basic page layout Add header, content, and footer components to app

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CountButton from './CountButton';
+
+const Content = () => {
+	return (
+		<div>
+			<CountButton />
+		</div>
+	);
+};
+
+export default Content;
---
+++ src/components/CountButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountButton = () => {
+	return <button>Click Me</button>;
+};
+
+export default CountButton;
