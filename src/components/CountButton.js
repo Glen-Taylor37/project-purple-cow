b06d7c27543627701a0aecf74327a759e4afd612
Glen <gtaylor37@gmail.com>
2021-06-18 09:43:03 -0400

    Add social media buttons with links to Fearless

--- src/components/CountButton.js
+++ src/components/CountButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { incrementCount, getCount } from '../apis/countApi';
-const CountButton = () => {
+
+const CountButton = (props) => {
 	return (
 		<Button onClick={() => incrementCount()} variant="contained" color="primary">
 			Click Me
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,60 @@ import { Box } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import { Link } from '@material-ui/core';
 
-//import LinkedInIcon from '@material-ui/icons/LinkedIn';
-//import FacebookIcon from '@material-ui/icons/Facebook';
-// import TwitterIcon from '@material-ui/icons/Twitter';
-// import InstagramIcon from '@material-ui/icons/Instagram';
+import { IconButton, Button } from '@material-ui/core';
 
-const Copyright = () => {
-	return (
-		<Typography variant="body2" align="center">
-			{'Copyright © Cow'}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
-	);
-};
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import Facebook from '@material-ui/icons/Facebook';
+
+// const Copyright = () => {
+// 	return (
+// 		<Typography variant="body2" align="center">
+// 			{'Copyright © Cow'}
+// 			{new Date().getFullYear()}
+// 			{'.'}
+// 		</Typography>
+// 	);
+// };
 
 const Footer = () => {
 	return (
-		<Box bgcolor="primary.main" maxWidth="sm">
-			<Copyright />
+		<Box
+			display="flex"
+			bgcolor="primary.main"
+			maxWidth="sm"
+			justifyContent="flex-end"
+		>
+			<IconButton
+				variant="link"
+				href="https://twitter.com/fearlessbmore"
+				target="_blank"
+			>
+				<TwitterIcon color="secondary" fontSize="large" />
+			</IconButton>
+			<IconButton
+				variant="link"
+				href="https://www.facebook.com/fearlessbmore"
+				target="_blank"
+			>
+				<FacebookIcon color="secondary" fontSize="large" />
+			</IconButton>
+			<IconButton
+				variant="link"
+				href="https://www.instagram.com/fearlessbmore/"
+				target="_blank"
+			>
+				<InstagramIcon color="secondary" fontSize="large" />
+			</IconButton>
+			<IconButton
+				variant="link"
+				href="https://www.linkedin.com/company/fearlessbmore/"
+				target="_blank"
+			>
+				<LinkedInIcon color="secondary" fontSize="large" />
+			</IconButton>
 		</Box>
 	);
 };
